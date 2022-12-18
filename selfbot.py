c048a222aa5e7c222958e622c12289b2324c2f6b
head47 <63517545+head47@users.noreply.github.com>
2022-12-18 20:10:48 +0300

    selfbot: ,=!i n

---
+++ selfbot.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+import discord
+from config import *
+
+class Client(discord.Client):
+    async def on_ready(self):
+        print(f"Logged in as {self.user}!")
+    
+    async def on_message(self, message):
+        if message.author != self.user:
+            return
+        
+        if message.content.endswith(','):
+            await message.edit(content=message.content[:-1])
+            await message.channel.send('!i n')
+
+client = Client()
+client.run(TOKEN)
