f4aa3f8e503c704a1c846a94de3d273d1bef92c8
head47 <63517545+head47@users.noreply.github.com>
2022-12-19 05:29:39 +0300

    add db schema

---
+++ schema.sql
@@ -0,0 +1,12 @@
+-- items definition
+
+CREATE TABLE items (
+	id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+	name TEXT NOT NULL,
+	look TEXT NOT NULL,
+	"type" TEXT NOT NULL,
+	"attributes" TEXT NOT NULL,
+    durability INTEGER,
+    weight INTEGER,
+    slot TEXT
+);
