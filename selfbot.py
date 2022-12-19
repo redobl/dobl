71f9ad9bff275e3ddc77afa32c4b59965cdaf2fa
head47 <63517545+head47@users.noreply.github.com>
2022-12-19 20:40:39 +0300

    next, miss, roll macros for selfbot

--- selfbot.py
+++ selfbot.py
@@ -13,6 +13,19 @@ class Client(discord.Client):
         if message.content.endswith(','):
             await message.edit(content=message.content[:-1])
             await message.channel.send('!i n')
+        
+        elif message.content.startswith('-'):
+            if message.content == '-':
+                await message.delete()
+                await message.channel.send('!i n')
+            elif message.content == '-п':
+                await message.edit(content='*Промах.*')
+            elif message.content.startswith('-р'):
+                if len(message.content) == 2:
+                    thres = 50
+                else:
+                    thres = int(message.content[2:])
+                await message.channel.send(f'!r 1d100<={thres}')
 
 client = Client()
 client.run(TOKEN)
