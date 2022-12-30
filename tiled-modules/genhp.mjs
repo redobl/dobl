62cebae1d4a712d3372008fb88ea168b2e004b53
head47 <63517545+head47@users.noreply.github.com>
2022-12-30 23:19:33 +0300

    add GenHP button

---
+++ tiled-modules/genhp.mjs
@@ -0,0 +1,19 @@
+var action = tiled.registerAction("GenHP", function (action) {
+    var spentSP = tiled.prompt("How much SP spent on HP?");
+    var process = new Process();
+    var scriptPath = tiled.projectFilePath+"\\generate.py"
+
+    process.exec("python",[scriptPath,"hp","100",spentSP]);
+    var result = process.readLine();
+
+    var sel = tiled.activeAsset.selectedObjects;
+    if (sel.length == 1) {
+        sel[0].setProperty("Очки Здоровья", result+"/"+result+" ("+result+")")
+    }
+})
+
+action.text = "Generate HP"
+
+tiled.extendMenu("MapView.Objects", [
+    { action: "GenHP" }
+]);
